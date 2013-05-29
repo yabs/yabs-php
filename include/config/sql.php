ab64d9f8c3f131222ff4041aab0bb4878302baa8
Indy <indy@den.pa>
2013-05-29 14:17:19 -0700

    Example SQL Configuration

---
+++ include/config/sql.php
@@ -0,0 +1,12 @@
+<?php
+    /* SQL Configuration file
+     * 
+     *  Used for storing SQL credentials
+     */
+    
+    define("DB_HOST", "localhost");
+    define("DB_USER", "badger");
+    define("DB_PASS", "badgepassword");
+    define("DB_NAME", "badges");
+    
+?>
